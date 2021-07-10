44c95b3f4eb14a351938f6a75f9616e0b0c599c0
mini-skirt <mini-skirt@outlook.com>
2021-07-11 00:05:42 +0800

    Load confidential config; Improve code readability

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,11 +1,11 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
-use App\State as AppState;
 use DI\Container;
+use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use App\State as AppState;
 
 function app()
 {
@@ -16,12 +16,27 @@ function project_path()
 {
     return dirname(__DIR__);
 }
+
 $config = require project_path().'/config.php';
+
+if (is_file(project_path().'/config.secret.php')) {
+    $config_secret = require project_path().'/config.secret.php';
+} else {
+    $config_secret = [];
+}
+
+if ($config === 1 or $config === false) $config = [];
+if ($config_secret === 1 or $config === false) $config_secret = [];
+
 $container = new Container;
+
 AppFactory::setContainer($container);
 
 $container->set('view', function() use ($config) {
-    return Twig::create($config['dependency']['twig']['view'], ['cache' => $config['dependency']['twig']['cache']]);
+    return Twig::create(
+        $config['dependency']['twig']['view'], 
+        ['cache' => $config['dependency']['twig']['cache']]
+    );
 });
 
 AppState::setInstance(AppFactory::create());
