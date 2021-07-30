7ea0ec2ce557baa0b9bcab11b8b30e26402bfe22
Mini Skirt <mini-skirt@outlook.com>
2021-07-30 13:59:10 +0800

    Change code convention: Use full namespaced class name in usages

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,15 +1,9 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-use DI\Container;
-use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
-use App\State as AppState;
-
 function app()
 {
-    return AppState::getInstance();
+    return \App\AppState::getInstance();
 }
 
 function project_path()
@@ -28,20 +22,20 @@ if (is_file(project_path().'/config.secret.php')) {
 if ($config === 1 or $config === false) $config = [];
 if ($config_secret === 1 or $config === false) $config_secret = [];
 
-$container = new Container;
+$container = new \DI\Container;
 
-AppFactory::setContainer($container);
+\Slim\Factory\AppFactory::setContainer($container);
 
 $container->set('view', function() use ($config) {
-    return Twig::create(
+    return \Slim\Views\Twig::create(
         $config['dependency']['twig']['view'], 
         ['cache' => $config['dependency']['twig']['cache']]
     );
 });
 
-AppState::setInstance(AppFactory::create());
+\App\AppState::setInstance(AppFactory::create());
 
-app()->add(TwigMiddleware::createFromContainer(app()));
+app()->add(\Slim\Views\TwigMiddleware::createFromContainer(app()));
 
 require 'route.php';
 
