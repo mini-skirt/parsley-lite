9e0106152b57fdd6fdcd31fce83f552af5437bdd
mini-skirt <mini-skirt@outlook.com>
2021-09-06 20:08:13 +0800

    Add doc-comments

--- bootstrap/fn.php
+++ bootstrap/fn.php
@@ -1,16 +1,25 @@
 <?php
 namespace MiniSkirt\ParsleyLite;
 
+/**
+ * @return \Slim\App
+ */
 function app()
 {
     return \App\State::getInstance();
 }
 
+/**
+ * @return string
+ */
 function project_path()
 {
     return dirname(__DIR__);
 }
 
+/**
+ * @return \Psr\Http\Message\ResponseInterface
+ */
 function response($content = '', $contentType = 'text/html', $status = 200)
 {
     $resp = new \Slim\Psr7\Response;
@@ -22,6 +31,9 @@ function response($content = '', $contentType = 'text/html', $status = 200)
     return $resp;
 }
 
+/**
+ * @return \Psr\Http\Message\ResponseInterface
+ */
 function responsePlainText($content = '', $contentType = 'text/plain', $status = 200, $toStringHandler = null)
 {
     if (! is_string($content)) {
@@ -35,6 +47,9 @@ function responsePlainText($content = '', $contentType = 'text/plain', $status =
     return response($content, $contentType, $status);
 }
 
+/**
+ * @return \Psr\Http\Message\ResponseInterface
+ */
 function responseJson($content = '', $contentType = 'text/json', $status = 200, $encodeHandler = null)
 {
     $content = ($encodeHandler) ? $encodeHandler($content) : json_encode($content);
